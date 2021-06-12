1dad756b13ab81bc31a2e8b95afd2c14c786e8ec
lin <b07705049@ntu.edu.tw>
2021-06-12 15:36:14 +0800

    [infected-match] Implementation of bigtable operations

---
+++ backend/infected-match/db/db.go
@@ -0,0 +1,85 @@
+package db
+
+import (
+    "context"
+    // "fmt"
+    "log"
+    _ "github.com/joho/godotenv/autoload"
+    "cloud.google.com/go/bigtable"
+)
+
+type Trace struct {
+    Class   string
+    Place   string
+    Time    string
+}
+
+type TraceList []Trace
+
+var adminClient *bigtable.AdminClient
+var client *bigtable.Client
+var tables []string
+
+const tableName = "trace-infected"
+
+func sliceContains(list []string, target string) bool {
+    for _, s := range list {
+        if s == target {
+            return true
+        }
+    }
+    return false
+}
+
+func InitDB(ac *bigtable.AdminClient, c *bigtable.Client, t []string) {
+    adminClient = ac
+    client = c
+    tables = t
+}
+
+func HandleInfectedMatch(traceList TraceList) (TraceList, []error) {
+
+    ctx := context.Background()
+	var matchedTraceList TraceList
+    var errs []error
+
+    // Create table if it's not yet created
+
+    if !sliceContains(tables, tableName) {
+        log.Printf("Creating table %s", tableName)
+        if err := adminClient.CreateTable(ctx, tableName); err != nil {
+            log.Fatalf("Could not create table %s: %v", tableName, err)
+            return matchedTraceList, append(errs, err)
+        }
+    }
+
+    // Fetch
+
+    tbl := client.Open(tableName)
+
+    for _, trace := range traceList {
+		rowKey := trace.Time
+
+		row, err := tbl.ReadRow(ctx, rowKey)
+		if err != nil {
+			log.Fatalf("Could not read row: %v", err)
+			return matchedTraceList, append(errs, err)
+		}
+
+		for cf, itemList := range row {
+			if cf != trace.Class { continue }
+			for _, item := range itemList {
+				column := item.Column
+				if column != trace.Place { continue }
+				goto trace_matched
+			}
+		}
+
+		continue
+
+trace_matched:
+		matchedTraceList = append(matchedTraceList, trace)
+	}
+
+    return matchedTraceList, errs
+}
